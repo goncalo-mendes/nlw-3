72f5f9775e4abda362d65487023854dc3540bd40
goncalo-mendes <goncalo.mendes.dev@gmail.com>
2020-10-15 20:47:06 +0100

    inicio do projeto

---
+++ web/src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import './styles/global.css';
+import './styles/pages/landing.css';
+
+import logoImg from './images/Logo.svg';
+
+function App() {
+  return (
+    <div id="page-landing">
+      <div className="content-wrapper">
+      <img src={logoImg}alt="Happy"/>
+      <main>
+        <h1>Felicidade para o mundo</h1>
+        <p>Visite orfanatos e mude o de muitas crianças</p>
+      </main>
+
+      <div className="location">
+        <strong>Rio do Sul</strong>
+        <span>Santa Catarina</span>
+      </div>
+
+      <a href="" className="enter-app">
+        >
+      </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
